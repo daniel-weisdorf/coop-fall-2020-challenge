618e85b859cc5576ffd44b5b4a7d5348e998f988
Daniel Weisdorf <dw1236@gmail.com>
2020-07-10 14:06:22 -0400

    made all changes this didnt really need multiple commits

--- solution_javascript.js
+++ solution_javascript.js
@@ -1,14 +1,57 @@
+// Simplest way to implement undo and redo functionality is a stack formation
+// Performing actions adds them to the top of the undoStack (and clears the redoStack )
+// Undoing actions adds them to the redoStack
+
+// A highly optimized way to do this would be to only implement add, have subtract be implemented as a negative add,
+// and then your stacks are just lists of numbers without having to keep track of whether its an add or subtract
+// since it's inherent in the pos/negativity
+
 class EventSourcer {
   constructor() {
     this.value = 0;
+    this.undoStack = [];
+    this.redoStack = [];
   }
 
-  add(num) {}
-  subtract(num) {}
-  undo() {}
-  redo() {}
-  bulk_undo(num) {}
-  bulk_redo(num) {}
+  add(num) {
+    this.value += num;
+    this.undoStack.push(num * -1);
+    this.redoStack.shift();
+  }
+  subtract(num) {
+    this.add(num * -1);
+  }
+
+  // This code was duplicated since undo and redo are conceptually the same
+  // Took them out into a helper just to avoid duplication
+  undoRedoHelper(primaryStack, secondaryStack) {
+    if (primaryStack.length > 0) {
+      let num = primaryStack.pop();
+      this.value += num;
+      secondaryStack.push(num * -1);
+    }
+  }
+  undo() {
+    this.undoRedoHelper(this.undoStack, this.redoStack);
+  }
+  redo() {
+    this.undoRedoHelper(this.redoStack, this.undoStack);
+  }
+
+  // Assuming that a bulk undo of 5 actions when there's only 2 actions in the undo stack
+  // should still undo those 2 actions
+  bulk_undo(num) {
+    while (num > 0) {
+      this.undo();
+      num--;
+    }
+  }
+  bulk_redo(num) {
+    while (num > 0) {
+      this.redo();
+      num--;
+    }
+  }
 }
 
 // ----- Do not modify anything below this line (needed for test suite) ------
--- tests/javascript_solution.test.js
+++ tests/javascript_solution.test.js
@@ -1,6 +1,6 @@
-const EventSourcer = require('../solution_javascript');
+const EventSourcer = require("../solution_javascript");
 
-test('test add', () => {
+test("test add", () => {
   let sourcer = new EventSourcer();
 
   sourcer.add(2);
@@ -9,7 +9,7 @@ test('test add', () => {
   expect(sourcer.value).toEqual(7);
 });
 
-test('test subtract', () => {
+test("test subtract", () => {
   let sourcer = new EventSourcer();
 
   sourcer.subtract(2);
@@ -18,7 +18,7 @@ test('test subtract', () => {
   expect(sourcer.value).toEqual(-7);
 });
 
-test('test undo', () => {
+test("test undo", () => {
   let sourcer = new EventSourcer();
 
   sourcer.add(2);
@@ -30,19 +30,7 @@ test('test undo', () => {
   expect(sourcer.value).toEqual(6);
 });
 
-test('test redo', () => {
-  let sourcer = new EventSourcer();
-
-  sourcer.add(2);
-  sourcer.add(2);
-  sourcer.add(2);
-  sourcer.add(2);
-  sourcer.undo();
-
-  expect(sourcer.value).toEqual(6);
-});
-
-test('test redo', () => {
+test("test redo", () => {
   let sourcer = new EventSourcer();
 
   sourcer.add(5);
@@ -56,7 +44,7 @@ test('test redo', () => {
   expect(sourcer.value).toEqual(11);
 });
 
-test('test bulk undo', () => {
+test("test bulk undo", () => {
   let sourcer = new EventSourcer();
 
   sourcer.add(1);
@@ -69,7 +57,7 @@ test('test bulk undo', () => {
   expect(sourcer.value).toEqual(3);
 });
 
-test('test bulk redo', () => {
+test("test bulk redo", () => {
   let sourcer = new EventSourcer();
 
   sourcer.add(1);
@@ -81,12 +69,12 @@ test('test bulk redo', () => {
   sourcer.undo();
   sourcer.undo();
   sourcer.undo();
-  sourcer.bulk_redo(4)
+  sourcer.bulk_redo(4);
 
   expect(sourcer.value).toEqual(5);
 });
 
-test('test undo no actions', () => {
+test("test undo no actions", () => {
   let sourcer = new EventSourcer();
 
   sourcer.undo();
@@ -94,7 +82,7 @@ test('test undo no actions', () => {
   expect(sourcer.value).toEqual(0);
 });
 
-test('test redo with no redo step', () => {
+test("test redo with no redo step", () => {
   let sourcer = new EventSourcer();
 
   sourcer.redo();
@@ -102,7 +90,7 @@ test('test redo with no redo step', () => {
   expect(sourcer.value).toEqual(0);
 });
 
-test('test bulk undo overstep', () => {
+test("test bulk undo overstep", () => {
   let sourcer = new EventSourcer();
 
   sourcer.add(5);
@@ -115,7 +103,7 @@ test('test bulk undo overstep', () => {
   expect(sourcer.value).toEqual(0);
 });
 
-test('test bulk redo overstep', () => {
+test("test bulk redo overstep", () => {
   let sourcer = new EventSourcer();
 
   sourcer.add(5);
@@ -128,5 +116,5 @@ test('test bulk redo overstep', () => {
   sourcer.undo();
   sourcer.bulk_redo(5);
 
-  expect(sourcer.value).toEqual(5);
+  expect(sourcer.value).toEqual(25);
 });
